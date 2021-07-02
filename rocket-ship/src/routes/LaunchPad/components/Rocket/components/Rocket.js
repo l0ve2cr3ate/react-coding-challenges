6c725969827e183cb0dd320d1c8b96d2180cf3af
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-07-02 16:51:54 +0200

    Add solution for rocket-ship challenge.

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/Rocket.js
@@ -1,18 +1,18 @@
-import React, { useState, Component } from 'react';
-import RocketCore from './RocketCore';
+import React, { useState, PureComponent } from "react";
+import RocketCore from "./RocketCore";
 
-export function FunctionalRocket() {
+export const FunctionalRocket = React.memo(function FunctionalRocket() {
   const [initialLaunchTime] = useState(Date.now());
 
   return <RocketCore initialLaunchTime={initialLaunchTime} />;
-}
+});
 
-export class ClassRocket extends Component {
+export class ClassRocket extends PureComponent {
   constructor() {
     super();
 
     this.state = {
-      initialLaunchTime: Date.now()
+      initialLaunchTime: Date.now(),
     };
   }
 
