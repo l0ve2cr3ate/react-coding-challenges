27265b15fbbe11ec643337a6b53497bd2b90bc3e
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-08-04 21:27:35 +0200

    fix: Key prop warning.

--- chatter/src/layouts/CoreLayout/components/IconBackground.js
+++ chatter/src/layouts/CoreLayout/components/IconBackground.js
@@ -16,18 +16,30 @@ function getRandomIcon() {
 function IconRow({ numberOfIcons }) {
   return (
     <div className="icon-background__row">
-      {[...new Array(numberOfIcons)].map(() => {
+      {[...new Array(numberOfIcons)].map((_, index) => {
         const icon = getRandomIcon();
 
         return (
           <i
+            key={index}
             aria-hidden="true"
             className={icon.name}
             style={{
-              transform: icon.noRotation ? void 0 : `rotate(${getRandomNumber(0, 360)}deg)`,
-              fontSize: `${getRandomNumber(icon.minSize || 10, icon.maxSize)}px`,
-              marginTop: `${getRandomNumber(-SPACING_MARGIN, SPACING_MARGIN)}px`,
-              marginLeft: `${getRandomNumber(-SPACING_MARGIN, SPACING_MARGIN)}px`,
+              transform: icon.noRotation
+                ? void 0
+                : `rotate(${getRandomNumber(0, 360)}deg)`,
+              fontSize: `${getRandomNumber(
+                icon.minSize || 10,
+                icon.maxSize
+              )}px`,
+              marginTop: `${getRandomNumber(
+                -SPACING_MARGIN,
+                SPACING_MARGIN
+              )}px`,
+              marginLeft: `${getRandomNumber(
+                -SPACING_MARGIN,
+                SPACING_MARGIN
+              )}px`,
             }}
           />
         );
@@ -43,9 +55,9 @@ export default function IconBackground() {
 
   return (
     <div className="icon-background">
-      {[...new Array(numberOfRows)].map(() => (
-        <IconRow numberOfIcons={numberOfElsPerRow} />
+      {[...new Array(numberOfRows)].map((_, index) => (
+        <IconRow key={index} numberOfIcons={numberOfElsPerRow} />
       ))}
     </div>
-  )
+  );
 }
